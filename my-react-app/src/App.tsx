d5531788bbbcfa2ff5c81bf7faedef1d3ee4e8d9
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2023-05-04 15:01:57 -0300

    first commit

---
+++ my-react-app/src/App.tsx
@@ -0,0 +1,10 @@
+import { InfoArea } from "./components/infoArea/InfoArea"
+
+function App() {
+
+  return (
+      <InfoArea/>
+  )
+}
+
+export default App
---
+++ my-react-app/src/components/infoArea/InfoArea.tsx
@@ -0,0 +1,6 @@
+
+export const InfoArea = () => {
+  return (
+    <div>InfoArea</div>
+  )
+}
